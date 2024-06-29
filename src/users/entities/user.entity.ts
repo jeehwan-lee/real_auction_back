3b05b9bd054c67cc09bdaa9f47b82c4d301e880d
jeehwan <tjtojan@naver.com>
2024-06-30 03:03:11 +0900

    데이터베이스 설정

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('User')
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column({ length: 30 })
+  name: string;
+
+  @Column({ length: 60 })
+  email: string;
+
+  @Column({ length: 30 })
+  password: string;
+}
