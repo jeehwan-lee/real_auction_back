59dd3e14c7549299794a9f3370dab81136a4353b
jeehwan-lee <tjtojan@naver.com>
2024-07-16 20:40:01 +0900

    Guard를 통한 인증 적용

--- src/attendance/attendance.controller.ts
+++ src/attendance/attendance.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
 import { CreateAttendanceDto } from './dto/create-attendance.dto';
 import { DeleteAttendanceDto } from './dto/delete-attendance.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('attendance')
 export class AttendanceController {
   constructor(private attendanceService: AttendanceService) {}
 
+  @UseGuards(AuthGuard)
   @Post('/enter')
   async createAttendance(@Body() createAttendanceDto: CreateAttendanceDto) {
     return await this.attendanceService.createAttendance(createAttendanceDto);
   }
 
+  @UseGuards(AuthGuard)
   @Post('/exit')
   async deleteAttendance(@Body() deleteAttendanceDto: DeleteAttendanceDto) {
     return await this.attendanceService.deleteAttendance(deleteAttendanceDto);
--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -5,10 +5,11 @@ import { AttendaceEntity } from './entities/attendance.entity';
 import { AttendanceController } from './attendance.controller';
 import { NoticeService } from 'src/notice/notice.service';
 import { NoticeEntity } from 'src/notice/entities/notice.entity';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AttendaceEntity, NoticeEntity])],
-  providers: [AttendanceService, NoticeService],
+  providers: [AttendanceService, NoticeService, AuthService],
   controllers: [AttendanceController],
 })
 export class AttendanceModule {}
