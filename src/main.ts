54d44ed1a03c8fb51dece63b81402423e228ca68
jeehwan-lee <tjtojan@naver.com>
2024-07-17 14:00:25 +0900

    운영 배포 시 CORS policy 문제 해결

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONT_REACT_APP_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
