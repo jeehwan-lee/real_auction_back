e1ae92a53653a5976a5d55c96ac6bc0cbe2f35e7
jeehwan-lee <tjtojan@naver.com>
2024-07-18 14:16:54 +0900

    healthy check API 생성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('/healthy')
+  async getHealthy(@Res() res: Response) {
+    return { message: 'This is a 200 response' };
+  }
 }
